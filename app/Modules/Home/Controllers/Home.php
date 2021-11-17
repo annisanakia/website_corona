f63de48e31c3e0edd6bb842ae905aa87d9425755
Nakia <nakia@iglobal.co.id>
2021-11-17 14:37:26 +0700

    Home Controller

--- app/Modules/Home/Controllers/Home.php
+++ app/Modules/Home/Controllers/Home.php
@@ -15,24 +15,18 @@ use Illuminate\Support\Facades\Redirect;
 class Home extends Controller
 {
 
-    public function __construct()
-    {
-        // $this->middleware('auth');
-    }
-
-    public function comingSoon()
-    {
-        return view('Home::coming_soon');
-    }
-
     public function index(Request $request)
     {
+        //total data terkonfirmasi penyebaran covid-19
         $terkonfirmasi = App\Models\covid19_case::sum('terkonfirmasi');
+        //total data dirawat penyebaran covid-19
         $dirawat = App\Models\covid19_case::sum('dirawat');
+        //total data sembuh penyebaran covid-19
         $sembuh = App\Models\covid19_case::sum('sembuh');
+        //total data meninggal penyebaran covid-19
         $meninggal = App\Models\covid19_case::sum('meninggal');
 
-        $with['active'] = 1;
+        $with['active'] = 1; //menu navbar atas
         $with['terkonfirmasi'] = $terkonfirmasi;
         $with['dirawat'] = $dirawat;
         $with['sembuh'] = $sembuh;
@@ -40,13 +34,19 @@ class Home extends Controller
         return view('Home::index',$with);
     }
 
+    //list data penyebaran
     public function covid19_case(Request $request)
     {
+        //list data penyebaran covid-19
         $datas = App\Models\covid19_case::get();
 
+        //total data terkonfirmasi penyebaran covid-19
         $terkonfirmasi = App\Models\covid19_case::sum('terkonfirmasi');
+        //total data dirawat penyebaran covid-19
         $dirawat = App\Models\covid19_case::sum('dirawat');
+        //total data sembuh penyebaran covid-19
         $sembuh = App\Models\covid19_case::sum('sembuh');
+        //total data meninggal penyebaran covid-19
         $meninggal = App\Models\covid19_case::sum('meninggal');
 
         $with['active'] = 2;
@@ -58,6 +58,7 @@ class Home extends Controller
         return view('Home::covid19_case',$with);
     }
 
+    //kehalaman form tambah data penyebaran
     public function covid19_case_create(Request $request)
     {
         $with['active'] = 2;
@@ -66,25 +67,36 @@ class Home extends Controller
 
     public function covid19_case_save(Request $request)
     {
+        //mengambil semua data inputan
         $input = $request->all();
+
+        //set default dirawat 0 kalau kosong
         $input['dirawat'] = $request->input('dirawat') != ''? $request->input('dirawat') : 0;
+        //set default terkonfirmasi 0 kalau kosong
         $input['terkonfirmasi'] = $request->input('terkonfirmasi') != ''? $request->input('terkonfirmasi') : 0;
+        //set default sembuh 0 kalau kosong
         $input['sembuh'] = $request->input('sembuh') != ''? $request->input('sembuh') : 0;
+        //set default meninggal 0 kalau kosong
         $input['meninggal'] = $request->input('meninggal') != ''? $request->input('meninggal') : 0;
 
+        //get model database covid19_case
         $models = new App\Models\covid19_case();
         $validation = $models->validate($input);
 
+        //kalau validasi sukses
         if ($validation->passes()) {
-            $models->create($input);
-            return Redirect::route('home.covid19_case');
+            $models->create($input); // simpan data
+            return Redirect::route('home.covid19_case'); //redirect ke halaman penyebaran covid
         }
+
+        //redirect ke halaman tambah data penyebaran covid saat validasi gagal
         return Redirect::route('home.covid19_case_create')
             ->withInput()
             ->withErrors($validation)
             ->with('message', 'There were validation errors.');
     }
 
+    //kehalaman form edit data penyebaran
     public function covid19_case_edit($id, Request $request)
     {
         $data = App\Models\covid19_case::find($id);
@@ -93,6 +105,40 @@ class Home extends Controller
         return view('Home::covid19_case_edit',$with);
     }
 
+    public function covid19_case_update($id, Request $request)
+    {
+        //mengambil semua data inputan
+        $input = $request->all();
+
+        //set default dirawat 0 kalau kosong
+        $input['dirawat'] = $request->input('dirawat') != ''? $request->input('dirawat') : 0;
+        //set default terkonfirmasi 0 kalau kosong
+        $input['terkonfirmasi'] = $request->input('terkonfirmasi') != ''? $request->input('terkonfirmasi') : 0;
+        //set default sembuh 0 kalau kosong
+        $input['sembuh'] = $request->input('sembuh') != ''? $request->input('sembuh') : 0;
+        //set default meninggal 0 kalau kosong
+        $input['meninggal'] = $request->input('meninggal') != ''? $request->input('meninggal') : 0;
+
+        //get model database covid19_case
+        $models = new App\Models\covid19_case();
+        $validation = $models->validate($input);
+
+        //kalau validasi sukses
+        if ($validation->passes()) {
+            $data = $models->find($id); // get data berdasarkan id
+            $data->update($input); // update data
+            return Redirect::route('home.covid19_case'); //redirect ke halaman penyebaran covid
+        }
+
+        //redirect ke halaman edit data penyebaran covid saat validasi gagal
+        $with['id'] = $id;
+        return Redirect::route('home.covid19_case_edit',$with)
+            ->withInput()
+            ->withErrors($validation)
+            ->with('message', 'There were validation errors.');
+    }
+
+    //fungsi hapus data penyebaran berdasarkan id
     public function covid19_case_delete($id, Request $request)
     {
         $data = App\Models\covid19_case::find($id);
@@ -102,42 +148,20 @@ class Home extends Controller
         return Redirect::route('home.covid19_case');
     }
 
+    // list data variasi vaksin
     public function vaccine_variant(Request $request)
     {
-        $datas = App\Models\vaccine_variant::get();
+        $datas = App\Models\vaccine_variant::get(); //get data jenis vaksin covid-19
 
         $with['datas'] = $datas;
         $with['active'] = 3;
         return view('Home::vaccine_variant',$with);
     }
 
-    public function covid19_case_update($id, Request $request)
-    {
-        $input = $request->all();
-        $input['dirawat'] = $request->input('dirawat') != ''? $request->input('dirawat') : 0;
-        $input['terkonfirmasi'] = $request->input('terkonfirmasi') != ''? $request->input('terkonfirmasi') : 0;
-        $input['sembuh'] = $request->input('sembuh') != ''? $request->input('sembuh') : 0;
-        $input['meninggal'] = $request->input('meninggal') != ''? $request->input('meninggal') : 0;
-
-        $models = new App\Models\covid19_case();
-        $validation = $models->validate($input);
-
-        if ($validation->passes()) {
-            $data = $models->find($id);
-            $data->update($input);
-            return Redirect::route('home.covid19_case');
-        }
-
-        $with['id'] = $id;
-        return Redirect::route('home.covid19_case_edit',$with)
-            ->withInput()
-            ->withErrors($validation)
-            ->with('message', 'There were validation errors.');
-    }
-
+    // list data variasi covid-19
     public function covid19_variant(Request $request)
     {
-        $datas = App\Models\covid19_variant::get();
+        $datas = App\Models\covid19_variant::get(); //get data varian covid-19
 
         $with['datas'] = $datas;
         $with['active'] = 4;
